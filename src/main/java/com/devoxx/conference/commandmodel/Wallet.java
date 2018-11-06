555fc18c72ae6a612f314c182e37f70fa0ce3362
smcvb <smcvbeelen@gmail.com>
2018-11-06 15:40:22 +0100

    Add Wallet command/event API Introduce Wallet Aggregate

---
+++ src/main/java/com/devoxx/conference/commandmodel/Wallet.java
@@ -0,0 +1,60 @@
+package com.devoxx.conference.commandmodel;
+
+import com.devoxx.conference.coreapi.CreateWalletCommand;
+import com.devoxx.conference.coreapi.DepositCommand;
+import com.devoxx.conference.coreapi.DepositedEvent;
+import com.devoxx.conference.coreapi.NotEnoughFoundException;
+import com.devoxx.conference.coreapi.WalletCreatedEvent;
+import com.devoxx.conference.coreapi.WithdrawCommand;
+import com.devoxx.conference.coreapi.WithdrawnEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
+public class Wallet {
+
+    @AggregateIdentifier
+    private String walletId;
+    private int balance;
+
+    public Wallet() {
+    }
+
+    @CommandHandler
+    public Wallet(CreateWalletCommand command) {
+        AggregateLifecycle.apply(new WalletCreatedEvent(command.getWalletId(), command.getBalance()));
+    }
+
+    @CommandHandler
+    public void handle(DepositCommand command) {
+        AggregateLifecycle.apply(new DepositedEvent(walletId, command.getAmount()));
+    }
+
+    @CommandHandler
+    public void handle(WithdrawCommand command) throws NotEnoughFoundException {
+        int amount = command.getAmount();
+        if (balance - amount < 0) {
+            throw new NotEnoughFoundException();
+        }
+        AggregateLifecycle.apply(new WithdrawnEvent(walletId, amount));
+    }
+
+    @EventSourcingHandler
+    public void on(WalletCreatedEvent event) {
+        walletId = event.getWalletId();
+        balance = event.getBalance();
+    }
+
+    @EventSourcingHandler
+    public void on(DepositedEvent event) {
+        balance = balance + event.getAmount();
+    }
+
+    @EventSourcingHandler
+    public void on(WithdrawnEvent event) {
+        balance = balance - event.getAmount();
+    }
+}
---
+++ src/main/java/com/devoxx/conference/coreapi/WalletApi.kt
@@ -0,0 +1,33 @@
+package com.devoxx.conference.coreapi
+
+data class CreateWalletCommand(
+        val walletId: String,
+        val balance: Int
+)
+
+data class WithdrawCommand(
+        val walletId: String,
+        val amount: Int
+)
+
+data class DepositCommand(
+        val walletId: String,
+        val amount: Int
+)
+
+
+
+data class WalletCreatedEvent(
+        val walletId: String,
+        val balance: Int
+)
+data class WithdrawnEvent(
+        val walletId: String,
+        val amount: Int
+)
+data class DepositedEvent(
+        val walletId: String,
+        val amount: Int
+)
+
+class NotEnoughFoundException : Exception()
